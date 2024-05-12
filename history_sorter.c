bcb8b945693ef4c4978390c28b63348fda290682
reyuki <hikikomori.yukisan@gmail.com>
2024-05-12 13:12:29 +0700

    bulk update

--- history_sorter.c
+++ history_sorter.c
@@ -9,14 +9,24 @@
 // The main goal of the program is to reduce the size of the history file
 // by reviewing the sorted history file and manually decide to remove redundant command
 // thus make the file more smoll, and overwrite it manually after you've done on reviewing it.
-
+//
 // remove trailing space at the beginning of the command on each line
-// cat ~/.bash_history | sed -e 's/^[[:space:]]*//' > ~/.bash_history_trimmed
+// cat ~/.bash_eternal_history | sed -e 's/^[[:space:]]*//' > ~/.bash_eternal_history_trimmed
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define HISTORY_FILE "/home/reyuki/.bash_eternal_history_trimmed"
+
+int compare_strings(const void *a, const void *b)
+{
+	const char *str1 = *(const char **)a;
+	const char *str2 = *(const char **)b;
+
+	// TODO: order by unixtimestamp
+	return strcmp(str1+12, str2+12);
+}
 
 int main(void)
 {
@@ -26,61 +36,67 @@ int main(void)
 		return 1;
 	}
 
-	// make sure to adjust the allocated size with the size of file
-	char *loaded_buffer = calloc(1, 1024 * 1024);
-	char *shift = loaded_buffer;
-	while (1) {
-		if (fread(shift, 1, 1, file_history) == 0)
-			break;
-		shift++;
-	}
+	// Array to store strings pointers (dynamically allocated)
+	char **strings = NULL;
+	char *str = NULL;
+	int num_strings = 0;
+	int isinserted = 0;
+	size_t len = 0;
 
-	int incr = 0;
-	shift = loaded_buffer;
-	while (*shift != '\0') {
-		if (*shift == '#')
-			shift += 12;
-		if (*shift == '\n') {
-		}
-		incr++;
-		shift++;
-	}
-	printf("%d\n", incr);
-	exit(0);
+	// Read lines from the file and store them in the array
+	char line[256];
+	while (fgets(line, sizeof(line), file_history)) {
+		int current_line = strlen(line);
+		len += current_line;
 
-	char pattern[32] = {0};
-	char *next_ptr;
-	shift = loaded_buffer;
-	do {
-		// skip the timestamp
-		if (*shift == '#')
-			shift += 12;
+		if (current_line == 12
+				&& line[0] == '#'
+				&& isdigit(line[1])
+				&& isdigit(line[10])
+				&& isinserted) {
+			isinserted = 0;
+			len -= 12;
 
-		// ensure the command is on the beginning line
-		if (*(shift-1) == '\n') {
-			// mark command as pattern, renew pattern each time (...)
-			int i = 0;
-			while (*shift != ' ')
-				pattern[i++] = *(shift++);
-			pattern[i] = '\0';
-			printf("%s\n", pattern);
-			// put the next_ptr on the next newline
-			while (shift++) {
-				if (*shift == '\n') {
-					next_ptr = shift+1;
-					break;
-				}
+			// Remove trailing newline (if present)
+			if (len > 0 && str[len-1] == '\n')
+				str[len-1] = '\0';
+
+			strings = (char **)realloc(strings, ++num_strings * sizeof(char *));
+			if (!strings) {
+				// Handle realloc error
+				perror("realloc");
+				exit(1);
 			}
 
-			// determina swapping or not by:
-			// check if the next command are same or not
-			// while (*shift != ' ')
-		}
-		
-		shift++;
-	} while (*shift != '\0');
+			// Add the string pointer to the array
+			strings[num_strings - 1] = str;
 
-	// printf("%s\n", loaded_buffer);
+			str = malloc(12+1);
+			if (!str) {
+				perror("malloc");
+				exit(1);
+			}
+
+			strcpy(str, line);
+			len = 12;
+			continue;
+		} else
+			isinserted = 1;
+
+		str = realloc(str, len+1);
+		if (!str) {
+			perror("realloc");
+			exit(1);
+		}
+		strcat(str, line);
+	}
+
+	fclose(file_history);
+	qsort(strings, num_strings, sizeof(char *), compare_strings);
+	// Print the sorted strings
+	for (int i = 0; i < num_strings; i++)
+		printf("%s\n", strings[i]);
 
 	return 0;
 }
+
